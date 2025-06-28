cd1ba8df16dd2cf06d7173e71061a6eb7f524761
Paste Prosmana <pasteprosmana189@gmail.com>
2025-06-28 21:51:43 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
+const app = express();
+const port = 8080;
+import cors from 'cors';
 import env from 'dotenv';
 env.config();
 import express, { Request, Response } from 'express';
 import { connect } from './config/db';
-const app = express();
-const port = 8080;
 connect();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173/'],
+    methods: ['GET, POST'],
+  }),
+);
 
 import shortRouter from './routes/short.routes';
 app.get('/', (_req: Request, res: Response) => {
